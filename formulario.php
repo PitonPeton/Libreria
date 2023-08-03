bca52f6d501986f4395a92d2c1c8dba608799217
PitonPeton <magnothrilex@gmail.com>
2023-08-02 23:35:04 -0400

    terminado base

--- formulario.php
+++ formulario.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script>
-
+        
     </script>
     <title>Document</title>
 </head>
 
 <body>
-    <form>
+    <form method="POST" action="envio_formulario.php">
         
         <div class="form-group">
             <label for="txtnombre">Nombre</label>
