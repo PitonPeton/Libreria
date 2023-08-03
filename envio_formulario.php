46e6dfcdd50eba1a9bf777b9c08df502459f6e2f
PitonPeton <magnothrilex@gmail.com>
2023-08-02 23:35:19 -0400

    terminado

--- envio_formulario.php
+++ envio_formulario.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once "Connection/connection.php";
 
 date_default_timezone_set('America/Santo_Domingo');
@@ -9,10 +10,8 @@ $correo = $_POST['correo'];
 $asunto = $_POST['asunto'];
 $comentario = $_POST['comentario'];
 
+$query_insert = $bd->prepare("insert into contacto(fecha, correo, nombre, asunto, comentario)values(?, ?, ?, ?, ?);");
 
-$query_insert = "insert into contacto(fecha, correo, nombre, asunto, comentario)values(:fecha, :correo, :nombre, :asunto, :comentario)";
+$result = $query_insert->execute([$fecha, $correo, $nombre, $asunto, $comentario]);
 
-$stmt = $bd->prepare($query_insert);
-
-$stmt->execute(['fecha'=>$fecha,'correo'=>$correo,'nombre'=>$nombre,'asunto'=>$asunto,'comentario'=>$comentario]);
 ?>
