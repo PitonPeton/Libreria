a52937771879e64f730c8991dda56695a44b7de8
PitonPeton <magnothrilex@gmail.com>
2023-08-03 04:59:21 -0400

    terminado

--- autores.php
+++ autores.php
@@ -1,43 +1,235 @@
-<?php
-include_once "Connection/connection.php";
-$query = $bd->query("select * from autores");
-$autor = $query->fetchAll(PDO::FETCH_OBJ);
-?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
 
-<div>
-    <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Apellido</th>
-                <th scope="col">Telefono</th>
-                <th scope="col">Direccion</th>
-                <th scope="col">Ciudad/Estado</th>
-                <th scope="col">Pais</th>
-            </tr>
-        </thead>
-        <tbody>
+<head>
 
-            <?php
-            foreach ($autor as $data) {
-            ?>
+    <?php
+    include_once "Connection/connection.php";
+    $query = $bd->query("select * from autores");
+    $autor = $query->fetchAll(PDO::FETCH_OBJ);
+    ?>
 
-            <tr>
-                <td scope="row"><?php echo $data->id_autor ; ?></td>
-                <td><?php echo $data->nombre; ?></td>
-                <td><?php echo $data->apellido; ?></td>
-                <td><?php echo $data->telefono; ?></td>
-                <td><?php echo $data->direccion; ?></td>
-                <td><?php echo $data->ciudad . "/" . $data->estado ?></td>
-                <td><?php echo $data->pais; ?></td>
-            </tr>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-            <?php
-            }
-            ?>
+    <title>Autores</title>
 
-        </tbody>
-    </table>
-</div>
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Jordani PC</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Listas
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="autores.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Autores</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="libros.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Libros</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <div class="sidebar-heading">
+                Formularios
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Contacto</span></a>
+            </li>
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <h1 class="h3 mb-4 text-gray-800">Biblioteca</h1>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Lista de Autores</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nombre</th>
+                                            <th>Apellido</th>
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
+                                            <th>Ciudad/Estado</th>
+                                            <th>Pais</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nombre</th>
+                                            <th>Apellido</th>
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
+                                            <th>Ciudad/Estado</th>
+                                            <th>Pais</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+
+                                        <?php
+                                        foreach ($autor as $data) {
+                                        ?>
+
+                                        <tr>
+                                            <td>
+                                                <?php echo $data->id_autor; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->nombre; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->apellido; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->telefono; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->direccion; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->ciudad . "/" . $data->estado ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $data->pais; ?>
+                                            </td>
+                                        </tr>
+
+                                        <?php
+                                        }
+                                        ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Jordani Perez 2020-10704</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
